47ec72e1636b832429c708df51fe4ea0adf786cf
Anirudh <68897241+anii-3301@users.noreply.github.com>
2021-07-07 14:37:53 +0400

    Create indexscript.js

---
+++ indexscript.js
@@ -0,0 +1,18 @@
+function Darktheme(){ 
+	document.body.style.backgroundColor = "black"; // body
+	//designer:
+	document.getElementById("head-desc").style.color = "white";
+ 	document.getElementById("desc").style.color = "white";
+	// coder:
+	document.getElementById("head-desc2").style.color = "white";
+	document.getElementById("desc2").style.color = "white";
+}
+function Lightheme(){
+	document.body.style.backgroundColor = "white"; // body
+	//designer:
+	document.getElementById("head-desc").style.color = "black";
+ 	document.getElementById("desc").style.color = "black";
+	// coder:
+	document.getElementById("head-desc2").style.color = "black";
+	document.getElementById("desc2").style.color = "black";
+	}
